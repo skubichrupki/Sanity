f0541559680c43eafe5755e8d79573551d46cb33
= <skupinski420@outlook.com>
2023-08-30 01:57:13 +0200

    UNITY: Init Part 2
    Notes, Player Movement Script, gitignore

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // stuff we want to happen when the game starts - kind of like on ready in JS
+    void Start()
+    {
+        Debug.Log("Hi mom"); // console.log(''); -> Debug.Log("");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
