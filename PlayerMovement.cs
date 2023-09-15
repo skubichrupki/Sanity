88dbc90957f8c7f8e91271c6e3508bd9c528f6bc
michu <skupinski420@outlook.com>
2023-09-15 02:24:48 +0200

    SANITY: ground check, jump down

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -8,30 +8,33 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rigidbodyy;
     private Animator animator;
     private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider;
 
     // initiate variables
-    [SerializeField] private float dirYUpSpeed = 12f;
-    [SerializeField] private float dirXMultiplier = 8f;
+    [SerializeField] private float dirYUpSpeed = 15f;
+    [SerializeField] private float dirYDownSpeed = 10f;
+    [SerializeField] private float dirXMultiplier = 10f;
     [SerializeField] private float dirXSpeed = 0f;
+    [SerializeField] private LayerMask groundJump;
 
     // STATES START
+    private enum MovementState { idle, run, jump, fall }
     // enum = group of values that variable of MovementState data type can hold
     // so: enum -> MovementState is data type, with those values for the variables {value1, value2}
-    private enum MovementState { idle, run, jump, fall }
     // values inside curly braces have int ID's. idle = 0 etc
-
-
     // This is done to manage state with ints instead of many booleans like IsRunning
     // fe: if State = 0, then idle in Unity
+
+    private bool IsGrounded;
 
     // Start is called BEFORE the first frame update
     private void Start()
     {
-        Debug.Log("Hi mom");
         // get components from Unity
         rigidbodyy = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider = GetComponent<BoxCollider2D>();    
     }
 
     private void Update()
@@ -44,15 +47,21 @@ public class PlayerMovement : MonoBehaviour
 
     private void MovementY() 
     {
-
-        if (Input.GetKeyDown(KeyCode.W))
+        // jump up
+        if (Input.GetKeyDown(KeyCode.W) && IsGroundedCheck())
         {
-            Debug.Log("Key: W");
+            Debug.Log("Jump Up");
             // Vector2(x,y)
             rigidbodyy.velocity = new Vector2(rigidbodyy.velocity.x, dirYUpSpeed);
 
             // Input.GetKeyDown(KeyCode.W))
             // Input.GetButtonDown("Up")
+        }
+        // jump down
+        else if (Input.GetKeyDown(KeyCode.S) && IsGroundedCheck() == false) 
+        {
+            Debug.Log("Jump Down");
+            rigidbodyy.velocity = new Vector2(rigidbodyy.velocity.x, -dirYDownSpeed);
         }
     }
 
@@ -110,6 +119,16 @@ public class PlayerMovement : MonoBehaviour
         // cast state into int with (int)x;
         // after all checks set value of state variable of type MovementState to corresponding int value of enum values
         animator.SetInteger("state", (int)state);
+    }
+
+    // check if player is grounded
+    private bool IsGroundedCheck()
+    {
+        // box cast - similar box to box collider, we move it down 0.1f and check if its overlapping with groundJump (ground layer)
+        IsGrounded = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down, .1f, groundJump);
+
+        // returns true if player touches the ground - can jump
+        return IsGrounded;
     }
 
 
